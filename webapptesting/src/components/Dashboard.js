5a963426edea7c3a9b662ea17d6182e7c6aa4a99
Jacob Williams <JaWilliamsDev@gmail.com>
2019-10-10 10:15:59 -0400

    made code dry

--- webapptesting/src/components/Dashboard.js
+++ webapptesting/src/components/Dashboard.js
@@ -6,7 +6,11 @@ const Dashboard = (props) => {
 	};
 
 	const handleStrike = () => {
-		props.strike >= 2 ? props.setStrike(0) || props.setOut(props.out + 1) : props.setStrike(props.strike + 1);
+        if (props.out === 2 && props.strike === 2 ) {
+            handleReset()
+        } else { 
+            props.strike >= 2 ? props.setStrike(0) || props.setOut(props.out + 1) : props.setStrike(props.strike + 1);
+    }
 	};
 
 	const handleFoul = () => {
@@ -17,24 +21,19 @@ const Dashboard = (props) => {
 		}
 	};
 
-	const handleHit = () => {
-		props.setStrike(0);
-		props.setBall(0);
-    };
-    
-    const handleOuts = () => {
+	const handleReset = () => {
         props.setStrike(0);
         props.setBall(0);
         props.setOut(0);
-    }
+    };
+    
 
 	return (
 		<div>
 			<button onClick={handleStrike}>Strike</button>
 			<button onClick={handleBall}>Ball</button>
 			<button onClick={handleFoul}>Foul</button>
-			<button onClick={handleHit}>Hit</button>
-            <button onClick={handleOuts}>Reset</button>
+			<button onClick={handleReset}>Hit</button>
 		</div>
 	);
 };
