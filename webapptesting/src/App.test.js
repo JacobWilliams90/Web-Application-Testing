b84f2b9dafbb7c7186edd93f43b9d350d2c990d1
Jacob Williams <JaWilliamsDev@gmail.com>
2019-10-10 16:24:22 -0400

    fixed testin library, MVP completed

--- webapptesting/src/App.test.js
+++ webapptesting/src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import App from './App.js';
+
+
+afterEach(rtl.cleanup);
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const wrapper = rtl.render(<App />);
+  wrapper.debug()
 });
+
